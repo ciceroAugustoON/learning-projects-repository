ae3d25172402f8e47ebcc26158195c894e1a69c6
ciceroaon <augustocicero831@gmail.com>
2024-05-18 16:11:05 -0300

    java-chess-system: Chess layer and printing the board

---
+++ java-chess-system/src/Program/App.java
@@ -0,0 +1,11 @@
+package Program;
+
+import chess.ChessMatch;
+
+public class App {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+    }
+}
