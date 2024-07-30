f0461098e5adbd984a6c3de904f44a01c511f7c8
CiceroAON <augustocicero831@gmail.com>
2024-07-30 11:38:57 -0300

    java-jdbc-demo: Creating project and git repository

---
+++ java-jdbc-demo/src/Application.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+
+import db.DB;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+
+}
