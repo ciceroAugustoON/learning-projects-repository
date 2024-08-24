cce6a4263b58a4b58e08d531723b63618bdf3ce0
CiceroAON <augustocicero831@gmail.com>
2024-08-24 20:02:50 -0300

    java-web-service: init

---
+++ java-web-service/src/main/java/com/caon/java_web_service/JavaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.caon.java_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebServiceApplication.class, args);
+	}
+
+}
---
+++ java-web-service/src/test/java/com/caon/java_web_service/JavaWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caon.java_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
